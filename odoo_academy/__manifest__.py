93352fe2fb91e32a5df0679f951357a1db378c9d
Carlos Garafulic <cgarafulic@gmail.com>
2022-11-07 19:40:11 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,20 @@
+# _*_ config: utf-8 _*_
+
+{
+    'name': 'Odoo Academy',
+    'summary': """Academy app to manage training""",
+    'description': """
+        Academy Module to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    'author': 'Carlos Garafulic',
+    'website': 'https://www.clingdata.com',
+    'licence': 'LGPL-3',
+    'category': 'Training',
+    'version': '0.1.0',
+    'depends': ['base'],
+    'data': [],
+    'demo': []
+}
