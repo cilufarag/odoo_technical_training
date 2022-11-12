e90a3ca06a0542d14298575eee7dcbd7e2c87af5
Carlos Garafulic <cgarafulic@gmail.com>
2022-11-12 23:57:00 +0000

    [ADD] Validations

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,5 +1,6 @@
 # _*_ coding: utf-8 _*_
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     _name = 'academy.course'
@@ -10,3 +11,19 @@ class Course(models.Model):
     level = fields.Selection(string='Level',
     selection=[('beginner','Beginner'),('intermediate','Intermediate'),('advanced', 'Advanced')],copy=False)
     active = fields.Boolean(string='Active', default=True)
+    base_price = fields.Float(string='Base Price', default=0.00)
+    additional_fee = fields.Float(string='Additional Fee', default=10.00)
+    total_price = fields.Float(string='Total Price', readonly=True)
+    
+    @api.onchange(base_price, additional_fee)
+    def _onchange_total_price(self):
+        if self.base_price < 0:
+            raise UserError('Base price cannot be set as negative')
+        
+        self.total_price = self.base_price + self.additional_fee
+        
+    @api.constrains(additional_fee)
+    def _check_additional_fee(self):
+        for record in self:
+            if record.additional_fee < 10:
+                raise ValidationError('Additional Fee cannot be less than $10') 
