613e549c508389835233028765bcbff3abc1f597
Carlos Garafulic <cgarafulic@gmail.com>
2022-11-19 01:35:36 +0000

    Inherit from Sales

--- odoo_academy/__manifest__.py
+++ odoo_academy/__manifest__.py
@@ -14,12 +14,13 @@
     'license': 'LGPL-3',
     'category': 'Training',
     'version': '0.1.0',
-    'depends': ['base'],
+    'depends': ['sale'],
     'data': ['security/academy_security.xml', 
              'security/ir.model.access.csv',
              'views/academy_menu_items.xml',
              'views/course_views.xml',
-             'views/session_views.xml'
+             'views/session_views.xml',
+             'views/sale_views_inherit.xml'
             ],
     'demo': ['demo/academy_demo.xml']
 }
